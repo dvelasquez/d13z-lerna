655e26ca3eb0015d0e39efd7080699c6d570cbf9
Danilo Velasquez <danilo.velasquez@gmail.com>
2020-05-10 21:21:14 +0200

    build(setting-lerna): added canary release for this branch

--- ci/success.sh
+++ ci/success.sh
@@ -13,7 +13,20 @@ release () {
   npm run lerna:publish --yes
 }
 
+release_canary () {
+  echo "Releasing new version to NPM"
+  git checkout "$TRAVIS_BRANCH"
+  git reset --hard
+  npm run lerna:release --yes
+  npm run lerna:publish:canary --yes
+}
+
 if [[ "$TRAVIS_BRANCH" = "master" && "$TRAVIS_PULL_REQUEST" = "false" ]]; then
+  setup_git
+  release
+fi
+# The following script will only run for the branch "build/ci", so we can test our changes
+if [[ "$TRAVIS_BRANCH" = "build/ci" && "$TRAVIS_PULL_REQUEST" = "false" ]]; then
   setup_git
   release
 fi
