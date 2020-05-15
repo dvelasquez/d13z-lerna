3b87cb3fe3d06f2422b3a0169194a0ce25881c09
Danilo Velasquez <danilo.velasquez@gmail.com>
2020-05-15 09:47:57 +0200

    feat(testing-stuff): added changes to library a to test changes.

--- packages/library-a/lib/index.js
+++ packages/library-a/lib/index.js
@@ -2,6 +2,7 @@ import { libraryB } from "@d13z-lerna/library-b/lib";
 
 const libraryA = () => {
     console.log('Library A initialised')
+    console.info('Testing if this change triggers a release')
     libraryB()
 }
 
